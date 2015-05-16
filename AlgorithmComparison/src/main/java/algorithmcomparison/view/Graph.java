6438cd51111f0ce084bf771bc3b2a6c152af5270
Anil Sevici <anilsevici@hotmail.com>
2015-05-17 00:20:04 +0300

    implement plot graph for keycomparison

---
+++ AlgorithmComparison/src/main/java/algorithmcomparison/view/Graph.java
@@ -0,0 +1,114 @@
+package algorithmcomparison.view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import algorithmcomparison.algorithms.BubbleSort;
+import algorithmcomparison.algorithms.MergeSort;
+import algorithmcomparison.runtimetest.AlgorithmRuntimeTester;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Graph extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JRadioButton rdbtnNewRadioButton;
+	private static final int NUM_OF_ITERATIONS = 10;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Graph frame = new Graph();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private XYDataset createDataset()
+	{
+		final XYSeries series1 = new XYSeries("BubbleSort");
+		AlgorithmRuntimeTester test=new AlgorithmRuntimeTester(new BubbleSort());
+		for (int i = 1; i <= NUM_OF_ITERATIONS; i++) {
+			series1.add(i*1000, test.run(i*1000).getAverageKeyComparisons());
+		}
+		
+		
+		final XYSeries series2= new XYSeries("MergeSort");
+		AlgorithmRuntimeTester test2=new AlgorithmRuntimeTester(new MergeSort());
+		for (int i = 1; i <= NUM_OF_ITERATIONS; i++) {
+			series2.add(i*1000,test2.run(i*1000).getAverageKeyComparisons());
+		}
+		
+		final XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(series1);
+        dataset.addSeries(series2);
+        
+        return dataset;
+	}
+	
+	private JFreeChart createChart(final XYDataset dataset){
+		final JFreeChart chart=ChartFactory.createXYLineChart("AVERAGEKEYCOMPARISON", "INPUTSIZE", "AVGKEYCOMPARISON", dataset);
+		
+		chart.setBorderPaint(Color.YELLOW);
+		
+		final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        return chart;
+		
+	}
+	
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public Graph() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 659, 473);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		rdbtnNewRadioButton = new JRadioButton("KEYCOMPARISON");
+		rdbtnNewRadioButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				final XYDataset dataset = createDataset();
+				final JFreeChart chart = createChart(dataset);
+			    ChartFrame frame=new ChartFrame("KEYCOMPARISON", chart);
+			    frame.setVisible(true);
+			    frame.setSize(750,650);
+				
+			}
+		});
+		rdbtnNewRadioButton.setBounds(246, 284, 129, 23);
+		contentPane.add(rdbtnNewRadioButton);
+	}
+}
