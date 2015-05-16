80f5b684b5eac55a96c6a90408b5938996fe28f6
Ois <isik.erhan@hotmail.com>
2015-05-16 21:26:49 +0300

    Implemented RuntimeStatistics

---
+++ AlgorithmComparison/src/main/java/algorithmcomparison/runtimetest/RuntimeStatistics.java
@@ -0,0 +1,46 @@
+package algorithmcomparison.runtimetest;
+
+public class RuntimeStatistics {
+
+	private double averageRuntime;
+	private double stDevRuntime;
+	private double averageKeyComparisons;
+	private double stDevKeyComparisons;
+	
+	public RuntimeStatistics(int[] runtimeResults, int[] keyCompResults) {
+		averageRuntime = getAverage(runtimeResults);
+		stDevRuntime = getStDev(runtimeResults, averageRuntime);
+		averageKeyComparisons = getAverage(keyCompResults);
+		stDevKeyComparisons = getStDev(keyCompResults, averageKeyComparisons);
+	}
+	
+	public double getAverageRuntime() {
+		return averageRuntime;
+	}
+
+	public double getStDevRuntime() {
+		return stDevRuntime;
+	}
+
+	public double getAverageKeyComparisons() {
+		return averageKeyComparisons;
+	}
+
+	public double getStDevKeyComparisons() {
+		return stDevKeyComparisons;
+	}
+
+	private double getAverage(int[] array) {
+		int sum = 0;
+		for(int i : array)
+			sum += i;
+		return (double) sum / array.length;
+	}
+	
+	private double getStDev(int[] array, double avg) {
+		double tot = 0;
+		for(int i = 0; i < array.length; i++)
+			tot += Math.pow(array[i] - avg, 2);
+		return Math.sqrt(tot);
+	}
+}
