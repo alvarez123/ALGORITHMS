874448bc10d5dc78a053c0fb71a39a2983940640
Anil Sevici <anilsevici@hotmail.com>
2015-05-17 14:18:31 +0300

    mergesort problem

--- AlgorithmComparison/src/main/java/algorithmcomparison/algorithms/MergeSort.java
+++ AlgorithmComparison/src/main/java/algorithmcomparison/algorithms/MergeSort.java
@@ -5,15 +5,21 @@ public class MergeSort implements SortAlgorithm {
 	private long runtimeMilliseconds;
 	private int numberOfKeyComparisons;
 	
+	private int[] numbers;
+	private int[] helper;
+	private int number;
+	
 	public MergeSort() {
 		reset();
 	}
 
 	public void sort(final int[] array) {
-		int[] tempArray = new int[array.length];
-		System.arraycopy(array, 0, tempArray, 0, array.length);
+		this.numbers=array;
+		number=array.length;
+		this.helper = new int[number];
+		//System.arraycopy(array, 0, numbers, 0, array.length);
 		final long startTime = System.nanoTime();
-		mergeSort(tempArray);
+		mergeSort(0,number-1);
 		runtimeMilliseconds = System.nanoTime() - startTime;
 	}
 
@@ -30,7 +36,52 @@ public class MergeSort implements SortAlgorithm {
 		numberOfKeyComparisons = 0;
 	}
 	
-	private int[] mergeSort(final int[] array) {
+	private void mergeSort(int low, int high) {
+	    // check if low is smaller then high, if not then the array is sorted
+	    if (low < high) {
+	      // Get the index of the element which is in the middle
+	      int middle = low + (high - low) / 2;
+	      // Sort the left side of the array
+	      mergeSort(low, middle);
+	      // Sort the right side of the array
+	      mergeSort(middle + 1, high);
+	      // Combine them both
+	      merge(low, middle, high);
+	    }
+	  }
+
+	  private void merge(int low, int middle, int high) {
+
+	    // Copy both parts into the helper array
+	    for (int i = low; i <= high; i++) {
+	      helper[i] = numbers[i];
+	    }
+
+	    int i = low;
+	    int j = middle + 1;
+	    int k = low;
+	    // Copy the smallest values from either the left or the right side back
+	    // to the original array
+	    while (i <= middle && j <= high) {
+	      if (helper[i] <= helper[j]) {
+	        numbers[k] = helper[i];
+	        i++;
+	      } else {
+	        numbers[k] = helper[j];
+	        j++;
+	      }
+	      k++;
+	    }
+	    // Copy the rest of the left side of the array into the target array
+	    while (i <= middle) {
+	      numbers[k] = helper[i];
+	      k++;
+	      i++;
+	    }
+
+	  }
+	
+	/*private int[] mergeSort(final int[] array) {
 		if(array.length == 1)
 			return array;
 		int mid = array.length / 2;
@@ -66,5 +117,5 @@ public class MergeSort implements SortAlgorithm {
 		}
 		
 		return dest;
-	}
+	}*/
 }
